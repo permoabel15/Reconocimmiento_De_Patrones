e6f1889dfefca34b763ca9573b147ae61250b39c
Abel Perez Laptop <105014433+permoabel15@users.noreply.github.com>
2022-11-25 18:26:37 -0600

    Create PCA.py

---
+++ PCA.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+iris = pd.read_csv('csv/wdbcEntrenamiento.csv')
+print(iris)
+features = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'A11', 'A12', 'A13', 'A24', 'A15', 'A16',
+            'A17', 'A18', 'A19', 'A20', 'A21', 'A22', 'A23', 'A24', 'A25', 'A26', 'A27', 'A28', 'A29', 'A30']
+target = ['Clase']
+
+
+X = iris[features]
+y = iris[target]
+target_names = ['setosa', 'virginica', 'versicolor']
+
+pca = PCA(n_components=4)
+X_r = pca.fit(X).transform(X)
+
+
+# Porcentaje de varianza explicado por cada componente
+print(
+    "Explicación de cada componente: %s"
+    % str(pca.explained_variance_ratio_)
+)
+
+i1 = iris.loc[:, 'Clase'] == 1
+i2 = iris.loc[i1]
+i3 = iris.loc[:, 'Clase'] == 2
+i4 = iris.loc[i3]
+i5 = iris.loc[:, 'Clase'] == 3
+i6 = iris.loc[i5]
+plt.scatter(i2.iloc[:, 0], i2.iloc[:, 1], label='Iris-setosa')
+plt.scatter(i4.iloc[:, 0], i4.iloc[:, 1], label='Iris-versicolor')
+plt.scatter(i6.iloc[:, 0], i6.iloc[:, 1], label='Iris-virginica')
+plt.title('Atributo A2 vs A3')
+plt.xlabel('A2')
+plt.ylabel('A3')
+plt.legend()
+plt.show()
+
+i2 = X_r[i1]
+i4 = X_r[i3]
+i6 = X_r[i5]
+plt.scatter(i2[:, 0], i2[:, 1], label='Iris-setosa')
+plt.scatter(i4[:, 0], i4[:, 1], label='Iris-versicolor')
+plt.scatter(i6[:, 0], i6[:, 1], label='Iris-virginica')
+plt.title('Atributo Y1 vs Y2')
+plt.xlabel('A2')
+plt.ylabel('A3')
+plt.legend()
+plt.show()
+
+for x in features:
+    slope, intercept, r, p, std_err = stats.linregress(iris[x], iris['Clase'])
+    print(r)
